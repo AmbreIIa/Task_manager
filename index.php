31bdb4568b024e415b5383392843e40895428e8d
Ambrella <154675435+AmbreIIa@users.noreply.github.com>
2026-09-15 15:45:12 +0300

    1

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+    $appName = "Task Manager";
+    $taskTitle = "Вивчи основи PHP";
+    $taskTimeEstimate = 2;
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    
+</head>
+<body>
+
+    <header>
+
+        <h1><?= $appName ?></h1>
+
+        <ul>
+            <li>Завдання: <?= $taskTitle ?></li>
+            <li>Кількість годин на виконання: <?= $taskTimeEstimate ?></li>
+        </ul>
+
+    </header>
+
+    <main>
+
+
+    </main>
+
+</body>
+</html>
